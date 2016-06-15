24878db8223d244d38e424c57b8cdfad5fddeef7
Rigoberto Hernandez <jfkbeatkid@Rigobertos-MacBook-Pro.local>
2016-06-15 00:31:10 -0400

    Minor changes to home and about page, installed body-parser

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var express = require('express');
 // calls up express and tells the system it is a variable
-var server = express()
+var server = express();
 // labels the express function as server
+var bodyParser= require('body-parser');
+// calls the bodyparser function from express
+
+server.use(express.static(__dirname+'/public'));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
 
 server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
@@ -13,6 +19,10 @@ server.get('/about', function(request, response){
 
 server.get('/contact', function(request, response){
   response.sendFile('public/html/contact.html', {root: __dirname});
+});
+
+server.post('/contact', function(request, response){
+  response.json(request.body);
 });
 
 server.listen(8080);
